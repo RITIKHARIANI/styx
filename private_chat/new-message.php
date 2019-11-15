562987ccb9fbdfc3120e6ecf79d71891628c1420
RITIKHARIANI <ritikhariani@gmail.com>
2019-11-15 09:00:29 +0530

    added private chat system

---
+++ private_chat/new-message.php
@@ -0,0 +1,69 @@
+<div id="new-message">
+    <p class="m-header">New Message</p>
+        <p class="m-body">
+            <form method="post" align="center">
+                <input type="text" id="user_name" list="user" onkeyup="check_in_db()" class="message-input" placeholder="user_name" name="user_name"><br><br>
+                <textarea name="message" class="message-input" placeholder="Enter your message"></textarea>
+                <!-- available users -->
+                <datalist id="user"></datalist>
+                <br><br>
+                <input type="submit" id="send" value="send" name="send">
+                <button onclick="document.getElementById('new-message').style.display='none'">Cancel</button>
+            </form>
+        </p>
+        <p class="m-footer">click send</p>
+    <!--end of new msg-->
+</div>
+
+
+<?php 
+    require_once("connection.php");
+    if(isset($_POST['send']))
+    {
+        $sender_name=$_SESSION['username'];
+        $receiver_name=$_POST['user_name'];
+        $message=$_POST['message'];
+        $date= date("Y-m-d h:i:sa");
+
+        $q='INSERT INTO `messages` (`id`,`sender_name`,`receiver_name`,`message_text`,`date_time`)
+        VALUES("","'.$sender_name.'","'.$receiver_name.'","'.$message.'","'.$date.'")';
+        $r=mysqli_query($con,$q);
+        if($r)
+        {
+            header("location:index.php?user=".$receiver_name);
+        }
+        else{
+            echo $q;
+        }
+
+    }
+?>
+<script src="../jquery/jquery-3.4.1.min.js"></script>
+<script>
+    
+    document.getElementById("send").disabled = true;
+
+    function check_in_db()
+    {
+        var user_name = document.getElementById("user_name").value;
+        $.post("check_in_db.php",
+        {
+            user: user_name
+        },
+        function(data,status)
+        {
+            //alert(data);
+            if(data== '<option value="no user">'){
+                //if user is registered then button will work
+                document.getElementById("send").disabled = true;
+            }
+            else {
+                document.getElementById("send").disabled = false;
+            }
+            }
+            document.getElementById('user').innerHTML=data;
+        }
+        );
+    }
+</script>
+
---
+++ private_chat/right-col.php
@@ -0,0 +1,133 @@
+<div class="right-col-container">
+    <div id="messages-container">
+
+    <?php 
+
+        $no_message=false;
+
+        if(isset($_GET['user'])){
+            $_GET['user']= $_GET['user'];
+        }else{
+            //user variable is not in url bar
+            //so select last user , you have sent messagae
+            $q='SELECT `sender_name`, `receiver_name` FROM `messages`
+                WHERE `sender_name`="'.$_SESSION['username'].'"
+                OR `receiver_name`="'.$_SESSION['username'].'"
+                ORDER BY `date_time` DESC LIMIT 1';
+            $r=mysqli_query($con, $q);
+            if($r)
+            {
+                if(mysqli_num_rows($r)>0){
+                    while($row=mysqli_fetch_assoc($r)){
+                        $sender_name=$row['sender_name'];
+                        $receiver_name=$row['receiver_name'];
+
+                        if($_SESSION['username'] == $sender_name){
+                            $_GET['user']=$receiver_name;
+
+                        }else{
+                            $_GET['user'] = $sender_name;
+                        }
+                    }
+                }else{
+                    //this user hasnt sent any message
+                    echo 'no messages from you';
+                    $no_message=true;
+                }
+            }else{
+                echo $q;
+            }
+        }
+
+        if($no_message == false)
+        {
+        $q='SELECT *FROM `messages` WHERE `sender_name`="'.$_SESSION['username'].'"
+            AND `receiver_name` = "'.$_GET['user'].'"
+            OR
+            `sender_name`="'.$_GET['user'].'" AND `receiver_name`="'.$_SESSION['username'].'" ';
+        $r=mysqli_query($con,$q);
+
+        if($r)
+        {
+            while($row=mysqli_fetch_assoc($r))
+            {
+                $sender_name=$row['sender_name'];
+                $receiver_name=$row['receiver_name'];
+                $message=$row['message_text'];
+
+                //check who the sender is
+                if($sender_name== $_SESSION['username'])
+                {
+                    //show grey back
+                    ?>
+                    <div class="grey-message">
+                        <a href="#">Me</a>
+                        <p><?php echo $message; ?> </p>
+                    </div>
+
+                    <?php
+                }else{
+                    ?>
+                    <div class="white-message">
+                        <a href="#"><?php echo $sender_name; ?></a>
+                        <p> <?php echo $message; ?> </p>
+                    </div>
+                    <?php
+                }
+            }
+        }else{
+            echo $q;
+        }
+    
+    }//end of no_message 
+
+    ?>
+       
+
+        <!--end of message container -->
+    </div>
+    <form method="post" id="message-form">
+    
+    <textarea id="message_text" placeholder="Enter your message" class="textarea"></textarea>
+    </form>
+
+    <!--end of right - col - container -->
+</div>
+
+<script src="../jquery/jquery-3.4.1.min.js"></script>
+
+<script>
+    $document.ready(function(event){
+
+        $("#right-col-container").on('submit','#message-form',function(){
+            var message_text=$('#message_text').val();
+        
+        //send the data to send to sending_process.php
+        $.post("sending_process.php?user=<?php echo $_GET['user']; ?>",
+        {
+            text:message_text,
+        },
+        //in return we get 
+        function(data,status){
+            //remove text from message_text
+
+            $("#message_text").val("");
+
+            //now adding data to message container
+            document.getElementById("messages-container").innerHTML +=data;
+        }
+        );
+    });
+        //if any button is clicked inside right container
+        $("#right-col-container").keypress(function(e){
+
+        if(e.keyCode ==13 && !e.shiftKey){
+            //enter is clicked without shift key
+            $("#message-form").submit();
+        }
+        
+        });
+    });
+
+
+</script>
